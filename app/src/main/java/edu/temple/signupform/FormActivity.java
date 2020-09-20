f6ec5cca914984ff3de812a0b7af0780b8fe19eb
CodeDccc <46765760+CodeDccc@users.noreply.github.com>
2020-09-21 00:28:49 +0100

    added some functionality

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,8 +3,12 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
     EditText name;
@@ -12,10 +16,22 @@ public class FormActivity extends AppCompatActivity {
     EditText password;
     EditText confirm;
     Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.namet);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirm = findViewById(R.id.confirm);
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(name.getText().toString()))
+                    Toast.makeText(FormActivity.this, "TEST AGAIN", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
